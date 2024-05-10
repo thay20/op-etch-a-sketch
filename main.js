da330f1e18cb1925ae2506a813b03b183477d4e9
thay20 <thay20@proton.me>
2024-05-10 00:23:48 -0400

    Add RGB color and increasing transparency
    The draw color is now in randomized RGB and the cells start at 10%
    transparency and increase 10% on each mouseover.

--- main.js
+++ main.js
@@ -39,11 +39,30 @@ function sketch() {
     }
   }
 
+  function changeRGBA(e) {
+    let red = Math.floor(Math.random()*256);
+    let green = Math.floor(Math.random()*256);
+    let blue = Math.floor(Math.random()*256);
+    let slice = Array.from(e.style.backgroundColor).slice(-2,-1);
+    let alpha;
+
+    if ((e.style.backgroundColor.match(/,/g) || []).length == 2 || slice == 9) {
+      alpha = "1.0";
+    } else if ((e.style.backgroundColor.match(/,/g) || []).length == 3){
+      slice++; 
+      alpha = `0.${slice}`;
+    } else {
+      alpha = "0.1";
+    }
+
+    e.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+    console.log(e.style.backgroundColor);
+  }
+
 
   const sketchPad = document.getElementById("sketch-pad")
   const buttonReset = document.getElementById("reset")
   const buttonGridSize = document.getElementById("grid-size")
-
   let gridSize = 16;
 
   buttonGridSize.addEventListener("click", () => getGridSize())
@@ -51,7 +70,7 @@ function sketch() {
   
   sketchPad.addEventListener("mouseover", (e) => {
     if (e.target && e.target.matches("div.grid-cell")) {
-      e.target.classList.add("grid-color");
+      changeRGBA(e.target);
     }
   })
 
